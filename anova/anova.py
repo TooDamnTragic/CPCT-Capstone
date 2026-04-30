898ce282065efa59f54232599b437d75ef793b98
Finnegan Cappelli <frc822@rit.edu>
2026-04-29 21:47:33 -0400

    final commit spring 2026

--- anova/anova.py
+++ anova/anova.py
@@ -34,3 +34,42 @@ anova_results = pd.DataFrame(results, columns=['Variable', 'F-statistic', 'p-val
 anova_results = anova_results.sort_values('p-value')
 
 print(anova_results.head(20))
+
+# output all results to CSV
+anova_results.to_csv('anova_results.csv', index=False)
+
+df['AI_authority_mean'] = df[['Q24_1','Q24_2','Q24_3','Q24_4','Q24_5']].mean(axis=1) # average of 5 questions about AI authority
+
+def classify_ai_role(x):
+    if x <= 2:
+        return "Tool"
+    elif x <= 3.5:
+        return "Collaborator"
+    else:
+        return "Teammate"
+
+df['AI_role'] = df['AI_authority_mean'].apply(classify_ai_role)
+
+df['AI_teammate_value_mean'] = df[['Q27','Q28','Q29']].mean(axis=1) # average of 3 questions about AI teammate value
+
+df['cohesion'] = df[['OC-1','OC-2','OC-3','OC-4','OC-5']].mean(axis=1) # average of 5 questions about organizational cohesion
+
+df['cognitive_diversity'] = df[['CD-1','CD-2','CD-3','CD-4']].mean(axis=1) # average of 4 questions about cognitive diversity
+
+df['self_diversity'] = df[['Differing abilities_1','Differing abilities_2','Differing abilities_3']].mean(axis=1) # average of 3 questions about self-perceived diversity
+
+AI_authority_mean ~ cohesion + cognitive_diversity
+
+# OR 
+
+AI_teammate_value_mean ~ cohesion + cognitive_diversity
+
+df['AI_usage_group'] = pd.qcut(df['AI_authority_mean'], 3, labels=['Low','Medium','High'])
+
+df['AI_usage_group'] = pd.qcut(df['AI_teammate_value_mean'], 3, labels=['Low','Medium','High'])
+
+from scipy.stats import f_oneway
+
+groups = [g['cohesion'].dropna() for _, g in df.groupby('AI_usage_group')]
+f_oneway(*groups)
+
